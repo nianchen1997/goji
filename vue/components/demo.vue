2adb92fe23e69bb8b9c4bf6495418ce68d7e6f57
nianchen1997 <54983013+nianchen1997@users.noreply.github.com>
2023-04-23 12:00:26 +0800

    Revert "念宸面试题提交"

---
+++ vue/components/demo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="demo">this is a {{ name }}</div>
+  <RETableDemo/>
+</template>
+<style scoped>
+.demo {
+  color: red
+}
+</style>
+<script>
+import RETableDemo from './RETable/demo.vue'
+
+export default {
+  components: {RETableDemo},
+  data() {
+    return {
+      name: "demo"
+    }
+  }
+}
+</script>
